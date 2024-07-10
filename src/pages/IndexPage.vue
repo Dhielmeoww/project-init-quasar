f1dd86bc741482270098df2920ef04bf6aa6a3b2
smf-fadhil-it <fadhil.farhisyi@smartfinance.co.id>
2024-07-10 08:52:53 +0700

    initial project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    <p class="tw-text-3xl">tessssssssssssss</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
